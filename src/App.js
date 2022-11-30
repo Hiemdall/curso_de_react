9212fc97dfec8099ad5649034a797af3565e182f
Heimdall Rojas <soporte@integratic.com.co>
2022-11-30 09:26:02 -0500

    colocar imagen dentro de la apliaaccccccccccccccccccccccccccacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logointegratic from './imagenes/logo-fondoblanco.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <did className='integratic-logo-contenedor'>
+        <img
+        src={logointegratic}
+        className='integratic-logo'
+        alt='Logo de Integrtic'/>
+
+      </did>
     </div>
   );
 }
